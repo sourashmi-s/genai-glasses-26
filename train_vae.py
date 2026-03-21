9a7ce019fbf5c04d81e239aea010024c76c3e7aa
cfcmadlad <173536959+cfcmadlad@users.noreply.github.com>
2026-03-22 05:18:23 +0530

    Update train_vae.py

--- train_vae.py
+++ train_vae.py
@@ -16,10 +16,10 @@ FILTER_SIZE    = 3
 NUM_LAYERS     = 3
 ACTIVATION     = "elu"
 DECODER_TYPE   = "interpolation"
-NUM_RES_BLOCKS = 2
-BETA           = 0.13
+NUM_RES_BLOCKS = 1
+BETA           = 0.20
 LR             = 1e-3
-NUM_EPOCHS     = 100
+NUM_EPOCHS     = 50
 PERC_WEIGHT    = 0.05
 
 
